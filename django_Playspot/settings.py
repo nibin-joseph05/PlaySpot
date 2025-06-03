769ab4753733009108e0dded34d706c33216b93b
Nibin Joseph <nibin.joseph.career@gmail.com>
2025-06-03 18:08:31 +0530

    Update settings.py

--- django_Playspot/settings.py
+++ django_Playspot/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = False
+
 from pathlib import Path
 import os
 from dotenv import load_dotenv  # Import dotenv
